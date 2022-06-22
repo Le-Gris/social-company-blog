aa5c3d8c0b3173d3a83add779bdeae610d7441c9
alephG <solimlegris@gmail.com>
2022-06-22 00:09:53 -0400

    user forms and profile pic upload

--- social_company_blog/__init__.py
+++ social_company_blog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -15,6 +16,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app, db)
+
+########################
+### LOGIN CONFIGS
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = "users.login"
 
 from social_company_blog.core.views import core
 from social_company_blog.error_pages.handlers import error_pages
